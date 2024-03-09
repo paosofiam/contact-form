12bc31f2ae3766f0d0d0be78b67331005af7d4ec
Paola Sofia <ev_mendoza@outlook.com>
2024-03-08 20:12:22 -0600

    corrección de index.html y submit.php

--- submit.php
+++ submit.php
@@ -1,24 +1,26 @@
 <?php 
-    $datos = $_POST;
-	sendEmail($data);
+    ini_set( 'display_errors', 1);
+    error_reporting(E_ALL);
+    
+    $data1 = $_POST;
+	sendEmail($data1);
 	/*header('location: gracias.html');*/
-
-    function sendEmail($data){
-		$emailCC = $_POST['email'];
-		$to ="eva.mendoza1525@gmail.com;";
+    function sendEmail($data1){
+		/*$emailCC = $_POST['email'];*/
+		$to = "ev_mendoza@outlook.com";
 		$subject = 'Mailer HTML';
 		$message = "Este código ha funcionado. Datos: <b>MASTER, CONTROL
         & HARMONIZE DATA</b>";
 		$message .= "<ul>";
 		$fields = array("formName", "formMail", "formSubject", "formMessage");
 		foreach ($fields as $value) {
-			$message .= "<li>$value: $data[$value]</li>";
+			$message .= "<li>$value: $data1[$value]</li>";
 		}
-		$mensaje .= "</ul>";
+		$message .= "</ul>";
 		$headers = "MIME-Version: 1.0" . "\r\n";
 		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 		$headers .= 'From: Paola Sofia <noreply@gmail.com>' . "\r\n";
-		$headers .= "Cc: $emailCC" . "\r\n";
-		mail($to,$subject,$mensaje,$headers);
+		/*$headers .= "Cc: $emailCC" . "\r\n";*/
+		mail($to,$subject,$message,$headers);
     }
 ?>
